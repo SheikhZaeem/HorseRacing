a9b376a3bca8250dd87b2f2cf5dfb24443cac788
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2025-05-29 19:46:19 +0300

    Calculations For race round

--- src/components/RaceRound.vue
+++ src/components/RaceRound.vue
@@ -19,3 +19,58 @@
     </div>
   </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+import { useHorseStore } from '../store';
+import HorseCard from './Horse.vue';
+
+export default {
+  name: 'RaceRound',
+  components: { HorseCard },
+  props: {
+    roundIndex: { type: Number, required: true }
+  },
+
+  setup(props) {
+
+    const store = useHorseStore();
+    const round = computed(() => store.schedule[props.roundIndex]);
+    const roundNumber = computed(() => props.roundIndex + 1);
+
+    const roundHorses = computed(() =>
+      round.value.horses.map(id => store.allHorses.find(h => h.id === id))
+    );
+
+    //compute track length
+    const maxDistancePx = 800;
+    const maxMeters = 2200;
+    const trackLength = computed(() =>
+      Math.round((round.value.distance / maxMeters) * maxDistancePx)
+    );
+
+    const rawTimes = computed(() => store.roundTimes[props.roundIndex] || []);
+
+    // finidng  minimum time (fastest horse)
+    const minRaw = computed(() => {
+      if (!rawTimes.value.length) return 1;
+      return Math.min(...rawTimes.value.map(rt => rt.rawTime));
+    });
+
+    function getDurationFor(horseId) {
+      const entry = rawTimes.value.find(rt => rt.id === horseId);
+      if (!entry) return 5;
+      return (entry.rawTime / minRaw.value) * 5;  // Increased to 5 seconds base
+    }
+    
+    return {
+      round,
+      roundNumber,
+      roundHorses,
+      trackLength,
+      getDurationFor
+    };
+  }
+};
+
+</script>
