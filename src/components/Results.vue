cf3d17c8e564ad1b556c513b55e91d6a9bab0985
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2025-05-29 20:04:02 +0300

    Create final results

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="results">
+    <h3>Results</h3>
+    <ul>
+      <li v-for="entry in results" :key="entry.round">
+        Round {{ entry.round }} Winner: {{ entry.winnerName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useHorseStore } from '../store';
+
+export default {
+  name: 'RaceResults',
+  setup() {
+    const store = useHorseStore();
+    const results = computed(() => store.results);
+    return { results };
+  }
+};
+</script>
+
+<style scoped>
+.results {
+  margin-top: 20px;
+}
+.results ul {
+  list-style: none;
+  padding: 0;
+}
+.results li {
+  margin: 5px 0;
+}
+</style>
